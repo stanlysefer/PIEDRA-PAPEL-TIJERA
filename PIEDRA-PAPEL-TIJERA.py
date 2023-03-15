756504f7c6284b2684d9480476cec698fb622b23
sistemas <yerminzonsequeda@colegioguanenta.edu.co>
2023-03-15 10:20:30 -0500

    se creó el diagrama de flujo y se agregó al README

--- PIEDRA-PAPEL-TIJERA.py
+++ PIEDRA-PAPEL-TIJERA.py
@@ -1,1 +1,23 @@
 #programa para simular el juego de piedra,papel o tijera.
+import random
+
+print("------------------------------------------")
+print("---------LANZAMIENTO DE UN DADO----------")
+print("------------------------------------------")
+
+# input 
+
+# processing
+
+maquina = random.randint(1,3)
+
+# usuario = random.randint(1,3)
+
+jugador= int(input("escoje que sacaras:"))
+
+if (maquina==1): #piedra
+    if (jugador==1):
+        rta="empate"
+    elif (jugador==2):
+        rta= "ganaste"
+    else:
