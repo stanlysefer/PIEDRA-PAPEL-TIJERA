bb4b82d9903b9bae09f55105e84c5adbbe9f639e
stanlysefer <yerminzonsequeda@colegioguanenta.edu.co>
2023-03-16 22:10:51 -0500

    HICE EL PROGRAMA DE PYTHON

--- PIEDRA-PAPEL-TIJERA.py
+++ PIEDRA-PAPEL-TIJERA.py
@@ -1,23 +1,61 @@
 #programa para simular el juego de piedra,papel o tijera.
-import random
 
-print("------------------------------------------")
-print("---------LANZAMIENTO DE UN DADO----------")
-print("------------------------------------------")
+import random
 
 # input 
 
+print("----------------------------------------------")
+print("---------JUEGO PIEDRA PEPEL O TIJERA----------")
+print("----------------------------------------------")
+
 # processing
+
+
+jugador = int(input("Escoje lo que sacaras, ¡PERO RAPIDO!: "))
 
 maquina = random.randint(1,3)
 
-# usuario = random.randint(1,3)
+piedra=1
+papel=2
+tijera=3
 
-jugador= int(input("escoje que sacaras:"))
+if (maquina==1):#piedra
+    if (jugador==1):#piedra
+        rta="vaya vaya fue un empate"
+    elif (jugador==2):#papel
+        rta= "ganaste  wow"
+    elif (jugador==3):#tijera
+        rta= "---perdiste  jajaja"
 
-if (maquina==1): #piedra
-    if (jugador==1):
-        rta="empate"
-    elif (jugador==2):
-        rta= "ganaste"
-    else:
+
+if (maquina==2):#papel
+        if (jugador==1):#piedra
+            rta="---perdiste  jajaja"
+        elif (jugador==2):#papel
+            rta= "vaya vaya fue un empate"
+        elif (jugador==3):#tijera
+            rta= "ganaste  wow"
+
+
+if (maquina==3):#tijera
+        if(jugador==1):#piedra
+            rta="ganaste  wow"
+        elif(jugador==2):#papel
+            rta="---perdiste  jajaja"
+        elif(jugador==3):#tijera
+            rta="vaya vaya fue un empate"
+
+
+    
+if (jugador>3):
+    rta = "¡¡¡USTED DIGITÓ UN NÚMERO NO VALIDO!!!"
+
+if(jugador<3):
+    rta=print("la maquina saco: " +str(maquina))
+    rta=print("usted saco: " +str(jugador))
+
+# output
+print(rta)
+
+
+
